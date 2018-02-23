3d21fe18aae71d5e861680ef433712bdad4bddaa
Wizofgoz <wizofgoz2@gmail.com>
2018-02-23 12:17:17 -0800

    Updated dashboard route to controller action

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', \App\Http\Controllers\DashboardController::class.'@index')->name('dashboard');
 
 Route::resource('characters', 'CharacterController', ['except' => [
     'edit'
