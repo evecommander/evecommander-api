821079ef9b57d87cd4140f8819ae8f9000834506
Wizofgoz <wizofgoz2@gmail.com>
2018-05-15 14:01:03 -0700

    Initial definition of routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use CloudCreativity\LaravelJsonApi\Facades\JsonApi;
 use CloudCreativity\LaravelJsonApi\Routing\ApiGroup;
@@ -22,7 +21,7 @@ Route::post('logout', 'AuthController@logout');
 Route::post('refresh', 'AuthController@refresh');
 Route::post('me', 'AuthController@me');
 
-Route::get('auth/callback', 'CharacterController@callback');
+Route::get('auth/callback', 'CharacterController@callback')->middleware('auth.callback');
 
 JsonApi::register('v1', ['namespace' => 'Api', 'middleware' => 'json-api.auth:default'], function (ApiGroup $api, Registrar $router) {
     $api->resource('users', [
@@ -53,6 +52,74 @@ JsonApi::register('v1', ['namespace' => 'Api', 'middleware' => 'json-api.auth:de
     ]);
 
     $api->resource('corporations', [
+
+    ]);
+
+    $api->resource('alliances', [
+
+    ]);
+
+    $api->resource('coalitions', [
+
+    ]);
+
+    $api->resource('billing-conditions', [
+
+    ]);
+
+    $api->resource('comments', [
+
+    ]);
+
+    $api->resource('discounts', [
+
+    ]);
+
+    $api->resource('doctrines', [
+
+    ]);
+
+    $api->resource('fittings', [
+
+    ]);
+
+    $api->resource('fleets', [
+
+    ]);
+
+    $api->resource('fleet-types', [
+
+    ]);
+
+    $api->resource('handbooks', [
+
+    ]);
+
+    $api->resource('invoices', [
+
+    ]);
+
+    $api->resource('invoice-items', [
+
+    ]);
+
+    $api->resource('memberships', [
+
+    ]);
+
+    $api->resource('membership-fees', [
+
+    ]);
+
+    $api->resource('membership-levels', [
+
+    ]);
+
+    $api->resource('permissions', [
+
+    ]);
+
+    $api->resource('replacement-claims', [
 
     ]);
 });
